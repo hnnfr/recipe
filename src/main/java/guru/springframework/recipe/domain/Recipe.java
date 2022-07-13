f3e05ca4ece58a24c23021bb32ccf816e9b6766f
hnnfr <59726038+hnnfr@users.noreply.github.com>
2022-07-13 09:06:59 +0200

    Entities with OneToOne and OneToMany, ManyToOne relationships

---
+++ src/main/java/guru/springframework/recipe/domain/Recipe.java
@@ -0,0 +1,30 @@
+package guru.springframework.recipe.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+}
